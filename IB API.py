b907fd1ccbccb23850ee0a5a13fbf3acf27b4137
peter <peter.bogar@pandora.sk>
2021-02-18 15:18:30 +0100

    stock and options data

--- IB API.py
+++ IB API.py
@@ -20,12 +20,32 @@ print()
 # Real time data
 ib.reqMarketDataType(4)
 ticker = ib.reqMktData(aapl_contract)
+print('Apple stock real time data:')
 print(ticker.marketPrice())
+print()
 
 # Historical data
-historical_data_aapl = ib.reqHistoricalData(aapl_contract, '',  barSizeSetting='1 hour', durationStr='2 D',  whatToShow='TRADES',  useRTH=False)
+historical_data_aapl = ib.reqHistoricalData(aapl_contract, '',  barSizeSetting='1 hour', durationStr='1 D',  whatToShow='TRADES',  useRTH=False)
+hist_data = util.df(historical_data_aapl)
+hist_data.pop('average')
+hist_data.pop('barCount')
+print('Apple stock historical data:')
+print(hist_data)
 print()
+
+# Real time option data
+ib.reqMarketDataType(4)
+aapl_option_contract = Option('AAPL', '20210219', 130, 'C', 'SMART')
+ticker = ib.reqMktData(aapl_option_contract)
+print('Apple options real time data:')
+print(ticker.marketPrice())
+print()
+
+# Options historical data
+historical_data_aapl = ib.reqHistoricalData(aapl_option_contract, '',  barSizeSetting='1 hour', durationStr='1 D',  whatToShow='TRADES',  useRTH=False)
+print('Apple options historical data:')
 hist_data = util.df(historical_data_aapl)
 hist_data.pop('average')
 hist_data.pop('barCount')
 print(hist_data)
+print()
