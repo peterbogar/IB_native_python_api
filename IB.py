864becb656a92a88870bfe3cda15deb7e619efb4
peter <peter.bogar@pandora.sk>
2021-02-21 20:29:08 +0100

    Market and historical data working for stock and forex

--- IB.py
+++ IB.py
@@ -20,6 +20,9 @@ class TestApp(EWrapper, EClient):
     def tickSize(self, reqId, tickType, size):
         print("Tick Size. Ticker Id:", reqId, "tickType:", TickTypeEnum.to_str(tickType), "Size:", size)
 
+    def historicalData(self, reqId, bar):
+        print("HistoricalData. ", reqId, " Date:", bar.date, "Open:", bar.open, "High:", bar.high, "Low:", bar.low, "Close:", bar.close, "Volume:", bar.volume, "Count:", bar.barCount, "WAP:", bar.average)
+
 
 def main():
     app = TestApp()
@@ -32,6 +35,13 @@ def main():
     contract.primaryExchange = "NASDAQ"
     app.reqMarketDataType(2)
     app.reqMktData(1, contract, "", False, False, [])
+
+    # contract.symbol = "EUR"
+    # contract.secType = "CASH"
+    # contract.exchange = "IDEALPRO"
+    # contract.currency = "USD"
+    # app.reqHistoricalData(1, contract, "", "1 D", "1 min", "MIDPOINT", 0, 1, False, [])
+
     app.run()
 
 
