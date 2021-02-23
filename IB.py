211967c9ff21d4a241329e9356b242f8a6aa0369
peter <peter.bogar@pandora.sk>
2021-02-23 07:59:11 +0100

    functions

--- IB.py
+++ IB.py
@@ -1,7 +1,7 @@
 # Native IB API
 # Based from https://cdcdyn.interactivebrokers.com/webinars/TA-2018-TWS-Python-Receiving-Market-Data-Study-Notes.pdf
 # Market data and historical data
-# Stock and forex
+# Stock, forex and options
 
 
 from ibapi.client import EClient
@@ -10,51 +10,72 @@ from ibapi.contract import Contract
 from ibapi.ticktype import TickTypeEnum
 from threading import Timer
 from time import sleep
+import pandas as pd
+
+
+bid = 0
+ask = 0
+close = 0
 
 
 class TestApp(EWrapper, EClient):
     def __init__(self):
         EClient.__init__(self, self)
+        self.data = []
+        self.df = None
 
     def error(self, reqId, errorCode, errorString):
         print("Error: ", reqId, " ", errorCode, " ", errorString)
 
     def tickPrice(self, reqId, tickType, price, attrib):
-        print("Id:", reqId, "Type:", TickTypeEnum.to_str(tickType), "Price:", price, end=' ')
+        # print("Id:", reqId, "Type:", TickTypeEnum.to_str(tickType), "Price:", price, end=' ')
+        global bid, ask, close
+        if TickTypeEnum.to_str(tickType) == 'BID':
+            bid = price
+        if TickTypeEnum.to_str(tickType) == 'ASK':
+            ask = price
+        if TickTypeEnum.to_str(tickType) == 'CLOSE':
+            close = price
 
     # def tickSize(self, reqId, tickType, size):
     #     print("Tick Size. Ticker Id:", reqId, "tickType:", TickTypeEnum.to_str(tickType), "Size:", size)
 
-    def historicalData(self, reqId, bar):
-        print("Id", reqId, bar.date, "Open:", bar.open, "High:", bar.high, "Low:", bar.low, "Close:", bar.close, "Volume:")
+    # def historicalData(self, reqId, bar):
+    #     print("Id", reqId, bar.date, "Open:", bar.open, "High:", bar.high, "Low:", bar.low, "Close:", bar.close, "Volume:")
 
     def contractDetails(self, reqId, contractDetails):
-        print("contractDetails: ", reqId, " ", contractDetails)
+        print("Id:", reqId, contractDetails)
 
     def contractDetailsEnd(self, reqId):
         # print("\ncontractDetails End\n")
         pass
-
-    # def start(self):
-    #     contract = Contract()
-    #     contract.symbol = "AAPL"
-    #     contract.secType = "STK"
-    #     contract.exchange = "SMART"
-    #     contract.currency = "USD"
-    #     contract.primaryExchange = "NASDAQ"
-    #     order = Order()
-    #     order.action = "BUY"
-    #     order.totalQuantity = 10
-    #     order.orderType = "LMT"
-    #     order.lmtPrice = 185
-    #     self.placeOrder(self.nextOrderId, contract, order)
 
     def stop(self):
         self.done = True
         self.disconnect()
 
 
+    #############################
+    def historicalData(self, reqId, bar):
+        self.data.append(vars(bar));
+
+    def historicalDataUpdate(self, reqId, bar):
+        line = vars(bar)
+        # pop date and make it the index, add rest to df
+        # will overwrite last bar at that same time
+        self.df.loc[pd.to_datetime(line.pop('date'))] = line
+
+    def historicalDataEnd(self, reqId: int, start: str, end: str):
+        print("HistoricalDataEnd. ReqId:", reqId, "from", start, "to", end)
+        self.df = pd.DataFrame(self.data)
+        self.df['date'] = pd.to_datetime(self.df['date'])
+        self.df.set_index('date', inplace=True)
+    #########################################
+
+
 def create_contract(symbol, sec_type='STK', exchange='SMART', currency='USD'):
+    # stk_aapl = create_contract('AAPL')
+    # frx_eurusd = create_contract('EUR', 'CASH', 'IDEALPRO', 'USD')
     contract = Contract()
     contract.symbol = symbol
     contract.secType = sec_type
@@ -64,6 +85,7 @@ def create_contract(symbol, sec_type='STK', exchange='SMART', currency='USD'):
 
 
 def create_options_contract(symbol, exp, strike, right, exchange='SMART', currency='USD', multiplier='100'):
+    # opt_spy = create_options_contract('SPY', '20210319', '400', 'C')
     contract = Contract()
     contract.symbol = symbol
     contract.lastTradeDateOrContractMonth = exp
@@ -76,33 +98,56 @@ def create_options_contract(symbol, exp, strike, right, exchange='SMART', curren
     return contract
 
 
-def main():
+def contract_details(contract):
     app = TestApp()
     app.connect("127.0.0.1", 7496, 12)
     ticker_id = 1
 
-    # Create contract
-    stk_aapl = create_contract('AAPL')
-    frx_eurusd = create_contract('EUR', 'CASH', 'IDEALPRO', 'USD')
-    opt_spy = create_options_contract('SPY', '20210319', '400', 'C')
-
-    # Contract detail
-    # ContractDetails(Contract,marketName,minTick,orderTypes,validExchanges,underConId,longName,contractMonth,industry,category,subcategory,timeZoneId,tradingHours,liquidHours,evRule,evMultiplier,aggGroup,tagvalue
-    app.reqContractDetails(ticker_id, frx_eurusd)
-
-    # Market data
-    # reqMarketDataType: 1 (default)- live data, 2- frozen live data, 3- delayed data, 4- delayed frozen
-    # app.reqMarketDataType(2)
-    # reqMktData(tickerId, contract, genericTickList, snapshot, regulatorySnaphsot, mktDataOptions)
-    # app.reqMktData(ticker_id, opt_spy, '', True, False, [])
-
-    # Historical data
-    # reqHistoricalData (tickerId, contract, endDateTime, durationStr, barSizeSetting, whatToShow, useRTH, formatDate, keepUpToDate, chartOptions
-    # app.reqHistoricalData(ticker_id, opt_spy, '', '2 D', '1 hour', 'TRADES', False, 1, False, [])
+    # Desc: ContractDetails(Contract,marketName,minTick,orderTypes,validExchanges,underConId,longName,contractMonth,industry,category,subcategory,timeZoneId,tradingHours,liquidHours,evRule,evMultiplier,aggGroup,tagvalue
+    app.reqContractDetails(ticker_id, contract)
 
     Timer(2, app.stop).start()
     app.run()
 
 
-if __name__ == "__main__":
-    main()
+def market_price(contract):
+    # Print current market bid, ask, close
+    app = TestApp()
+    app.connect("127.0.0.1", 7496, 12)
+    ticker_id = 1
+
+    # Desc: reqMarketDataType: 1 (default)- live data, 2- frozen live data, 3- delayed data, 4- delayed frozen
+    app.reqMarketDataType(2)
+    # Desc: reqMktData(tickerId, contract, genericTickList, snapshot, regulatorySnaphsot, mktDataOptions)
+    app.reqMktData(ticker_id, contract, '', True, False, [])
+
+    Timer(2, app.stop).start()
+    app.run()
+
+    print()
+    print('Symbol:', contract.symbol)
+    print('Bid:', bid)
+    print('Ask:', ask)
+    print('Close:', close)
+
+
+def historical_data(contract):
+    # Historical data
+    app = TestApp()
+    app.connect("127.0.0.1", 7496, 12)
+    ticker_id = 1
+
+    # Desc: reqHistoricalData (tickerId, contract, endDateTime, durationStr, barSizeSetting, whatToShow, useRTH, formatDate, keepUpToDate, chartOptions
+    app.reqHistoricalData(ticker_id, contract, '', '2 D', '1 hour', 'TRADES', False, 1, False, [])
+
+    Timer(2, app.stop).start()
+    app.run()
+
+
+# Create contract
+stk_aapl = create_contract('AAPL')
+
+# contract_details(stk_aapl)
+# market_price(stk_aapl)
+historical_data(stk_aapl)
+
